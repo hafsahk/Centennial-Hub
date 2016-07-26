021bf9e8d018e55461536bb0ce386f0cf0ca772a
hafsahk <hfs.hmk@gmail.com>
2016-07-25 22:49:12 -0400

    new pages
    edited login page -session n authentication

--- CentennialHub/Login.aspx.cs
+++ CentennialHub/Login.aspx.cs
@@ -1,27 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Login : System.Web.UI.Page
+namespace CentennialHub
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class Login : System.Web.UI.Page
     {
-       // this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
-        if (!IsPostBack)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
-        }
-    }
 
-    protected void Button1_Click(object sender, EventArgs e)
-    {       
- 
-        Response.Redirect("adminHomepage.aspx");
-       
+        }
     }
 }
