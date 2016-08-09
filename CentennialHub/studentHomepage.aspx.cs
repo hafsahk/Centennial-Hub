518eeb40e6214c55e88681d7302eae8d0aa1a534
hafsahk <hfs.hmk@gmail.com>
2016-08-09 18:41:39 -0400

    code for getting current semester

--- CentennialHub/studentHomepage.aspx.cs
+++ CentennialHub/studentHomepage.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CentennialHub
 {
@@ -12,8 +15,24 @@ namespace CentennialHub
         protected void Page_Load(object sender, EventArgs e)
         {
             
-                Label1.Text = "Wellcome " + Session["id"];
+                Label1.Text = "Welcome " + Session["id"];
             
         }
+        public void GettingStudentdata()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["COMP231-Project"].ConnectionString);
+            conn.Open();
+            String gettingSemester = "select program,semester from student where stID='" + Session["id"].ToString() + "'";
+            SqlCommand cmd = new SqlCommand(gettingSemester,conn);
+            SqlDataReader semRdr = cmd.ExecuteReader();
+
+            while (semRdr.Read()) {
+                String program = Convert.ToString(semRdr["program"]);
+                String sem = Convert.ToString(semRdr["semester"]);//convert to integer
+
+                    }
+
+
+        } 
     }
 }
