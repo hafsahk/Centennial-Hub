02351e586bdd90da2ce81dcff2f64d8246372f50
hafsahk <hfs.hmk@gmail.com>
2016-08-10 23:14:37 -0400

    prog,sem,course

--- CentennialHub/studentHomepage.aspx.cs
+++ CentennialHub/studentHomepage.aspx.cs
@@ -13,17 +13,18 @@ namespace CentennialHub
     public partial class studentHomepage : System.Web.UI.Page
     {
         String program;
-        int currentSem,i;
+        SqlConnection conn;
+        int currentSem, i;
         List<String> courses = new List<String>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-                Label1.Text = "Welcome " + Session["id"];
-            
+
+            Label1.Text = "Welcome " + Session["id"];
+
         }
         public void GettingStudentdata()
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["COMP231-Project"].ConnectionString);
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["COMP231-Project"].ConnectionString);
             conn.Open();
             String gettingSemester = "select program,semester from student where stID='" + Session["id"].ToString() + "'";
             SqlCommand cmd = new SqlCommand(gettingSemester, conn);
@@ -33,18 +34,13 @@ namespace CentennialHub
             {
                 program = Convert.ToString(semRdr["program"]);
                 currentSem = Convert.ToInt16(semRdr["semester"]);//convert to integer
-     
+
             }
+            semRdr.Close();
+            Response.Write(program);
+            Response.Write(currentSem);
             conn.Close();
         }
-
-        //protected void LinkButton1_Click(object sender, EventArgs e)
-        //{
-        //    GettingStudentdata();
-        //    previousSemCourses();
-        //    Response.Write(courses);
-
-        //}
 
         protected void Button1_Click(object sender, EventArgs e)
         {
@@ -53,22 +49,19 @@ namespace CentennialHub
 
             for (int j = 0; j < courses.Count; j++)
             {
-                Response.Write(courses[j]);
+                Response.Write("\n"+courses[j]);
             }
+            conn.Close();
         }
 
-        //protected void Button1_Click(object sender, EventArgs e)
-        //{
-        //    GettingStudentdata();
-        //    previousSemCourses();
-        //}
 
         public void previousSemCourses()
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["COMP231-Project"].ConnectionString);
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["COMP231-Project"].ConnectionString);
             conn.Open();
-            for (i=1;i<=currentSem;i++) {
-               
+            for (i = 1; i <= currentSem; i++)
+            {
+
                 String gettingcourses = "select coursecode from studentrecord where stID='" + Session["id"].ToString() + "' AND (remarks='unmet' and semester=1)";
                 SqlCommand cmd = new SqlCommand(gettingcourses, conn);
                 SqlDataReader courseRdr = cmd.ExecuteReader();
@@ -76,12 +69,10 @@ namespace CentennialHub
                 {
                     courses.Add(Convert.ToString(courseRdr["coursecode"]));
                 }
-
-                conn.Close();
+                courseRdr.Close();
             }
-           
         }
-
-
-        } 
     }
+}
+          
+        
